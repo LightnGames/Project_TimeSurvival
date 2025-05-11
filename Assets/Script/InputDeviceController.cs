23678c97af6603a2755e9cdade6383a4dd389545
OkakuTakahira <takahira4649@gmail.com>
2025-05-12 00:44:44 +0900

    キャラクター移動とカメラのキーボードマウス対応
    - カメラ操作対応
    - Unity 標準の InputSystem を利用できるように
    - プレイヤー操作用の入力情報 PlayerControlInput を作成(PlayerInput
      という名前は Unity 標準機能にあるので混同しないように Control
      という名前）
    - ゲーム画面をクリックしたらカーソルを無効化、Esc で有効化するように

---
+++ Assets/Script/InputDeviceController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDeviceController : MonoBehaviour
+{    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            InputSystem.DisableDevice(Mouse.current);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
+        }
+        if(Input.GetMouseButtonDown(0)) {
+            InputSystem.EnableDevice(Mouse.current);            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+        }
+    }
+}
